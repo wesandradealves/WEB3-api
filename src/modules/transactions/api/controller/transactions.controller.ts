9152eb8809c60cbdad898d1711c8343c4ece63a8
Victor Leles <victorgleles@gmail.com>
2025-02-26 21:25:13 -0300

    ajustando a doc da rota

--- src/modules/transactions/api/controller/transactions.controller.ts
+++ src/modules/transactions/api/controller/transactions.controller.ts
@@ -1,7 +1,9 @@
 import { IGetTransactionsByWalletIdUseCase } from "@/domain/interfaces/use-cases/transactions/get.transactions.by.wallet.id.use-case";
 import { Body, Controller, Get, Inject } from "@nestjs/common";
-import { ApiOperation } from "@nestjs/swagger";
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { TransactionsResponseDto } from "../dtos/transactions.reponse.dto";
 
+@ApiTags('transactions')
 @Controller('transactions')
 export class TransanctionsController {
   constructor(
@@ -11,10 +13,22 @@ export class TransanctionsController {
 
   @Get('get-transactions-by-wallet')
   @ApiOperation({ summary: 'Get transactions by wallet id and username' })
+  @ApiResponse({ status: 200, description: 'Transactions retrieved successfully.', type: TransactionsResponseDto })
+  @ApiResponse({ status: 400, description: 'Invalid input.' })
+  @ApiResponse({ status: 404, description: 'Transactions not found.' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        walletId: { type: 'number', example: 1 },
+        username: { type: 'string', example: 'john_doe@teste.com.br' },
+      },
+    },
+  })
   async getTransactionsByWalletId(
     @Body('walletId') walletId: number,
     @Body('username') username: string
-  ) {
+  ): Promise<TransactionsResponseDto> {
     return await this.getTransactionsByWalletIdUseCase.execute(walletId, username);
   }
 }
