3c6bb85c7ae64a738ca784e9addfac43c8d11776
Winston Moreira <winston.m.jesus@gmail.com>
2025-03-14 17:43:00 -0300

    fix: ajuste em alguns pontos de log e retorno

--- src/infrastructure/repositories/auth.repository.ts
+++ src/infrastructure/repositories/auth.repository.ts
@@ -8,7 +8,13 @@ import {
   ISignInOperatorRequest,
 } from '@/domain/interfaces/repositories/auth.repository';
 import { TokenService } from '@/modules/auth/services/token.service';
-import { Inject, Injectable, Logger, UnauthorizedException } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -52,29 +58,29 @@ export class AuthRepository implements IAuthRepository {
 
       return result;
     } catch (e) {
+      this.logger.log('Erro ao validar o refresh token.', e);
       if (e instanceof UnauthorizedException) {
-        this.logger.log('Token ');
         throw e;
       }
 
-      this.logger.log('Erro ao validar o refresh token.', e);
-      throw e;
+      throw new InternalServerErrorException();
     }
   }
 
   async validate(id: string): Promise<void> {
     const user = await this.user.findOne({ where: { id } });
     if (!user) {
-      throw new Error('Usuário não localizado.');
+      throw new UnauthorizedException();
     }
   }
 
   async signInSendTwoFa(data: ISignInOperatorRequest): Promise<any> {
-    const user = await this.user.findOneBy({ email: data.username });
-    if (!user) {
-      throw new UnauthorizedException('Usuario não localizado.');
-    }
     try {
+      const user = await this.user.findOneBy({ email: data.username });
+      if (!user) {
+        throw new UnauthorizedException();
+      }
+
       await this.cognito.signIn(data);
 
       const twofaCode = this.create2faCode();
@@ -97,44 +103,57 @@ export class AuthRepository implements IAuthRepository {
         template: process.env.SES_2FA_TEMPLATE,
         templateData: { name: 'customer', random_2fa: twofaCode },
       });
-    } catch (error) {
-      console.log(error);
+    } catch (e) {
+      this.logger.log('Erro ao autenticar o usuário.', e);
+      if (e instanceof UnauthorizedException) {
+        throw e;
+      }
+
+      throw new InternalServerErrorException();
     }
   }
 
   async validateTwoFa(username: string, twoFa: number): Promise<any> {
-    const user = await this.user.findOneBy({ email: username });
-    if (!user) {
-      throw new Error();
+    try {
+      const user = await this.user.findOneBy({ email: username });
+      if (!user) {
+        throw new Error();
+      }
+
+      const existingRecord = await this.authMFA.findOne({
+        where: { userId: user.id, mfa: twoFa.toString() },
+      });
+
+      if (!existingRecord || existingRecord.mfa !== twoFa.toString()) {
+        throw new UnauthorizedException();
+      }
+
+      // Gerando token e refresh token
+      this.logger.log('Erro ao criar o token.');
+      const signin = await this.token.createTokenJwt({
+        id: user.id,
+        email: user.email,
+        profile: user.profile,
+      });
+
+      // Gravando dados de autenticação no banco
+      this.logger.log('Erro ao gravar os dados de login no banco.');
+      await this.storeAfterSignIn({
+        userId: user.id,
+        refreshToken: signin.refreshToken,
+      });
+
+      await this.authMFA.update({ userId: user.id }, { isValid: false });
+      return signin;
+    } catch (e) {
+      this.logger.log('Erro ao validar o código', e);
+
+      if (e instanceof UnauthorizedException) {
+        throw e;
+      }
+
+      throw new InternalServerErrorException();
     }
-
-    const existingRecord = await this.authMFA.findOne({
-      where: { userId: user.id, mfa: twoFa.toString() },
-    });
-    if (!existingRecord) {
-      throw new Error();
-    }
-    if (existingRecord.mfa !== twoFa.toString()) {
-      throw new UnauthorizedException('Código inválido.');
-    }
-
-    // Gerando token e refresh token
-    this.logger.log('Erro ao criar o token.');
-    const signin = await this.token.createTokenJwt({
-      id: user.id,
-      email: user.email,
-      profile: user.profile,
-    });
-
-    // Gravando dados de autenticação no banco
-    this.logger.log('Erro ao gravar os daos de login no banco.');
-    await this.storeAfterSignIn({
-      userId: user.id,
-      refreshToken: signin.refreshToken,
-    });
-
-    await this.authMFA.update({ userId: user.id }, { isValid: false });
-    return signin;
   }
 
   private create2faCode() {
