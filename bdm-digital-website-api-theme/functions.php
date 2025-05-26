4df37177186a34cc495d383cb4e3f76583fe73a6
Wesley Andrade (WES) <wesandradealves@gmail.com>
2025-05-26 18:59:07 -0300

    Update functions.php

--- bdm-digital-website-api-theme/functions.php
+++ bdm-digital-website-api-theme/functions.php
@@ -523,6 +523,24 @@ function my_custom_block_category($categories, $post) {
 }
 add_filter('block_categories_all', 'my_custom_block_category', 10, 2);
 
+add_filter('rest_post_dispatch', function ($response) {
+    if (is_wp_error($response)) {
+        return $response;
+    }
+    $data = $response->get_data();
+    $json = wp_json_encode($data);
+
+    // Substitui todas as ocorrências do domínio antigo pelo novo
+    $json = str_replace(
+        'http://177.71.161.215:8000',
+        'https://dev-bdm.dourado.cash:8000',
+        $json
+    );
+
+    $response->set_data(json_decode($json, true));
+    return $response;
+}, 10, 1);
+
 // Api Health
 
 function api_health_check() {
